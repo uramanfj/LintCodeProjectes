526eb35a7a5f35a44028ed0887c5888e74d07e4a
fj9011 <fj9011@163.com>
2019-01-03 16:49:08 +0800

    Build a solution project to solve lintcode386 problem

---
+++ lintcode386/lintcode386/solution.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+	/**
+	 * @param s: A string
+	 * @param k: An integer
+	 * @return: An integer
+	 */
+	int lengthOfLongestSubstringKDistinct(string &s, int k) {
+		// write your code here
+	}
+};
